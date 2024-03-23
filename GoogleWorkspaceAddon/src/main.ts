778498b387ef3009cd26a367e1e0fea6de85ab85
Amin Zabardast <zabardast.amin@metu.edu.tr>
2024-03-24 00:43:27 +0300

    Adding Menus [#3]

--- GoogleWorkspaceAddon/src/main.ts
+++ GoogleWorkspaceAddon/src/main.ts
@@ -1,20 +1,8 @@
-import { sum5 } from './utils'
+import { createMenu } from './menus'
 
+/**
+ * This function runs when 
+ */
 const onOpen = () => {
   createMenu()
 }
-
-const createMenu = () => {
-    SpreadsheetApp.getUi().createMenu("⚙️ Admin Settings")
-     .addItem("Setting A", "settingA")
-     .addItem("Setting B", "settingB")
-     .addToUi();
- }
- 
- const settingA = () => {
-   SpreadsheetApp.getActive().toast(`Sum is ${sum5()}`);
- }
- 
- const settingB = () => {
-   SpreadsheetApp.getActive().toast("You selected Setting B.");
- }
---
+++ GoogleWorkspaceAddon/src/menus.ts
@@ -0,0 +1,18 @@
+import { sum5 } from './utils'
+
+const createMenu = () => {
+    SpreadsheetApp.getUi().createMenu("⚙️ Admin Settings")
+     .addItem("Setting A", "settingA")
+     .addItem("Setting B", "settingB")
+     .addToUi();
+ }
+ 
+ const settingA = () => {
+   SpreadsheetApp.getActive().toast(`Sum is ${sum5()}`);
+ }
+ 
+ const settingB = () => {
+   SpreadsheetApp.getActive().toast("You selected Setting B.");
+ }
+
+ export { createMenu }
