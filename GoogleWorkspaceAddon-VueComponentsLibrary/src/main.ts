f90e1f7c2c94eca977789fad8f95a7ce87dcb023
Amin Zabardast <zabardast.amin@metu.edu.tr>
2024-05-08 18:04:55 +0300

    Working Component Library
    This includes the component that are in Setup Page

--- GoogleWorkspaceAddon-VueComponentsLibrary/src/main.ts
+++ GoogleWorkspaceAddon-VueComponentsLibrary/src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {
+    BlockButton,
+    LoadingOverlay,
+    MainBody,
+    PageSlide
+} from './components/index'
 
-createApp(App).mount('#app')
+export {
+    BlockButton,
+    LoadingOverlay,
+    MainBody,
+    PageSlide
+}
--- GoogleWorkspaceAddon-VueComponentsLibrary/vite.config.ts
+++ GoogleWorkspaceAddon-VueComponentsLibrary/vite.config.ts
@@ -1,18 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    viteSingleFile()
+    dts({
+      rollupTypes: true
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'VueComponentsLibrary',
+      fileName: 'ui-library',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: [
+        'vue'
+      ],
+      output: {
+        intro: 'import "./style.css";',
+        globals: {
+          vue: 'Vue'
+        }
+      }
+
     }
   }
 })
